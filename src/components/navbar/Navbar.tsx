a3fea155877d229965f7a0183552a74e3eca5ed8
GlebTc <bob88tikhonov@gmail.com>
2024-04-15 12:44:34 -0400

    Initial Commit

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import DesktopNav from "@/src/components/navbar/desktop/DesktopNav"
+
+const Navbar = () => {
+  return (
+    <nav>
+        <DesktopNav />
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/components/navbar/desktop/DesktopNav.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const DesktopNav = () => {
+    const componentName = 'DESKTOP_NAV'
+  return (
+    <div className={`${componentName}_MAIN_CONTAINER`}>DesktopNav</div>
+  )
+}
+
+export default DesktopNav
