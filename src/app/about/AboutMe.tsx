fc2f8da96a83637ce80fbad0f893818cf1e4b4aa
GlebTc <bob88tikhonov@gmail.com>
2024-04-19 17:08:58 -0400

    Portfolio Page Completed

--- src/app/about/AboutMe.tsx
+++ src/app/about/AboutMe.tsx
@@ -12,15 +12,25 @@ const AboutMe = () => {
       <Image
         src={portrait}
         alt='Portrait of Web Developer'
-        width={300}
-        height={400}
+        width={200}
+        height={300}
         className='floating_container'
       />
+
       <div className={`${componentName}_CONTENT_SECTION`}>
-        <h2 className='text-2xl font-bold my-4 border-b-2 border-cyan-900 w-[80%] md:w-[60%] mx-auto'>A Little About Me</h2>
+        <h2 className='text-2xl font-bold my-4 border-b-2 border-cyan-900 w-[80%] md:w-[60%] mx-auto'>
+          A Little About Me
+        </h2>
         <p className='md:px-12'>
-          Hello, my name is Gleb.  I am a software developer with a passion for creating websites and web
-          applications with over {yearsOfExperience} years of experience. I have started this journey because I wanted to build a website for my own business.  It turns out that I really enjoy the process of creating websites and web applications.  For the past {yearsOfExperience} years I have embarked on a journey that started with the classic "Hello World" to building modern, responsive, and accessible website and full stack applications using the latest technologies.  Let's talk about technologies I use and why I use them.
+          Hello, my name is Gleb. I am a software developer with a passion for
+          creating websites and web applications with over {yearsOfExperience}{' '}
+          years of experience. I have started this journey because I wanted to
+          build a website for my own business. It turns out that I really enjoy
+          the process of creating websites and web applications. For the past{' '}
+          {yearsOfExperience} years I have embarked on a journey that started
+          with the classic "Hello World" to building modern, responsive, and
+          accessible website and full stack applications using the latest
+          technologies. Let's talk about technologies I use and why I use them.
         </p>
       </div>
     </div>
