534520e2ba170985a7c1dd38c63593caf79bf2f6
GlebTc <bob88tikhonov@gmail.com>
2024-07-30 21:35:10 -0400

    Updated Sitemap and Blogs

--- src/app/blog/next-js/page.tsx
+++ src/app/blog/next-js/page.tsx
@@ -1,17 +1,19 @@
 import blogPosts from '@/src/utils/constants/blogPosts.json';
 import BlogPosts from '@/src/app/blog/BlogPosts';
 
-const SEO = () => {
-  const componentName = 'BLOG_POST_SEO';
+
+
+const NextJS = () => {
+  const componentName = 'BLOG_POST_NEXTJS';
   return (
     <div
       className={`${componentName}_MAIN_CONTAINER h-full flex justify-center items-center flex-col gap-4 py-10 px-4 text-center max-w-[1200px] mx-auto`}
     >
       <div>
-        <BlogPosts blog={blogPosts[1]} />
+        <BlogPosts blog={blogPosts[2]} />
       </div>
     </div>
   );
 };
 
-export default SEO;
+export default NextJS;
